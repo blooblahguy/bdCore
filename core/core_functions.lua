350217042f6cad8f30911e3a6acc08bfd8d60e96
Glade Lynch <blooblahguy@gmail.com>
2018-08-17 15:10:04 -0600

    adding uft function

--- core/core_functions.lua
+++ core/core_functions.lua
@@ -38,3 +38,40 @@ function in_table ( e, t )
 	end
 	return false
 end
+
+local function chsize(char)
+    if not char then
+        return 0
+    elseif char > 240 then
+        return 4
+    elseif char > 225 then
+        return 3
+    elseif char > 192 then
+        return 2
+    else
+        return 1
+    end
+end
+
+-- This function can return a substring of a UTF-8 string, properly handling
+-- UTF-8 codepoints.  Rather than taking a start index and optionally an end
+-- index, it takes the string, the starting character, and the number of
+-- characters to select from the string.
+
+local function utf8sub(str, startChar, numChars)
+  local startIndex = 1
+  while startChar > 1 do
+      local char = string.byte(str, startIndex)
+      startIndex = startIndex + chsize(char)
+      startChar = startChar - 1
+  end
+
+  local currentIndex = startIndex
+
+  while numChars > 0 and currentIndex <= #str do
+    local char = string.byte(str, currentIndex)
+    currentIndex = currentIndex + chsize(char)
+    numChars = numChars -1
+  end
+  return str:sub(startIndex, currentIndex - 1)
+end
