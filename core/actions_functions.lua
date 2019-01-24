9ebd85cb7480b63b80a1a422e33f86d41ced5e74
Glade Lynch <blooblahguy@gmail.com>
2019-01-24 14:41:44 -0700

    aliasing events and matching in libs

--- core/actions_functions.lua
+++ core/actions_functions.lua
@@ -1,25 +1,31 @@
 local bdCore, c, f = select(2, ...):unpack()
 
 -- custom events/hooks
-bdCore.events = {}
-function bdCore:hookEvent(event, func)
-	local events = split(event,",") or {event}
+bd_action_events = bd_action_events or {}
+function bd_add_action(event, func)
+	local events = strsplit(",", event) or {event}
+	events = type(events) == "table" and events or {event}
+
 	for i = 1, #events do
 		e = events[i]
-		if (not bdCore.events[e]) then
-			bdCore.events[e] = {}
+		if (not bd_action_events[e]) then
+			bd_action_events[e] = {}
 		end
-		bdCore.events[e][#bdCore.events[e]+1] = func
+		bd_action_events[e][#bd_action_events[e]+1] = func
 	end
 end
-
-function bdCore:triggerEvent(event,...)
-	if (bdCore.events[event]) then
-		for k, v in pairs(bdCore.events[event]) do
+function bd_do_action(event,...)
+	if (bd_action_events[event]) then
+		for k, v in pairs(bd_action_events[event]) do
 			v(...)
 		end
 	end
 end
+
+bdCore.hookEvent = bd_add_action
+bdCore.triggerEvent = bd_do_action
+
+
 
 function bdCore:redraw()
 	bdCore:triggerEvent("bdcore_redraw")
--- lib/bdConfigLib1.0.lua
+++ lib/bdConfigLib1.0.lua
@@ -89,24 +89,22 @@ bdConfigLib.arrow:SetTexCoord(0.9, 0.9, 0.9, 0.6)
 bdConfigLib.arrow:SetVertexColor(1,1,1,0.5)
 
 -- event system
--- custom events/hooks
-bdConfigLib.events = {}
+bd_action_events = bd_action_events or {}
 function bd_add_action(event, func)
 	local events = strsplit(",", event) or {event}
 	events = type(events) == "table" and events or {event}
 
 	for i = 1, #events do
 		e = events[i]
-		if (not bdConfigLib.events[e]) then
-			bdConfigLib.events[e] = {}
+		if (not bd_action_events[e]) then
+			bd_action_events[e] = {}
 		end
-		bdConfigLib.events[e][#bdConfigLib.events[e]+1] = func
+		bd_action_events[e][#bd_action_events[e]+1] = func
 	end
 end
-
 function bd_do_action(event,...)
-	if (bdConfigLib.events[event]) then
-		for k, v in pairs(bdConfigLib.events[event]) do
+	if (bd_action_events[event]) then
+		for k, v in pairs(bd_action_events[event]) do
 			v(...)
 		end
 	end
