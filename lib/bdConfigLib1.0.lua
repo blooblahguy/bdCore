76de9592bd3caaf38840c82bbc99b47768687b18
Glade Lynch <blooblahguy@gmail.com>
2019-02-06 16:06:51 -0700

    don't config loop on nil types

--- lib/bdConfigLib1.0.lua
+++ lib/bdConfigLib1.0.lua
@@ -700,44 +700,46 @@ local function RegisterModule(self, settings, configuration, savedVariable)
 	for k, conf in pairs(configuration) do
 		-- loop through the configuration table to setup, tabs, sliders, inputs, etc.
 		for option, info in pairs(conf) do
-			if (settings.persistent) then
-				-- if variable is `persistent` its not associate with a profile
+			if (info.type) then
+				if (settings.persistent) then
+					-- if variable is `persistent` its not associate with a profile
+					
+					if (svc.persistent[settings.name][option] == nil) then
+						if (info.value == nil) then
+							info.value = {}
+						end
+
+						svc.persistent[settings.name][option] = info.value
+					end
+				else
+					-- this is a per-character configuration
+					-- print(settings.name, option)
+					-- print(svc.profile[settings.name])
+					svc.profile[settings.name] =  svc.profile[settings.name] or {}
+					if (svc.profile[settings.name][option] == nil) then
+						if (info.value == nil) then
+							info.value = {}
+						end
+
+						svc.profile[settings.name][option] = info.value
+					end
+				end
+
+				-- force blank callbacks if not set
+				info.callback = info.callback or settings.callback or function() return end
 				
-				if (svc.persistent[settings.name][option] == nil) then
-					if (info.value == nil) then
-						info.value = {}
-					end
-
-					svc.persistent[settings.name][option] = info.value
+				-- If the very first entry is not a tab, then create a general tab/page container
+				if (info.type ~= "tab" and #module.tabs == 0) then
+					module:CreateTab("General")
 				end
-			else
-				-- this is a per-character configuration
-				-- print(settings.name, option)
-				-- print(svc.profile[settings.name])
-				svc.profile[settings.name] =  svc.profile[settings.name] or {}
-				if (svc.profile[settings.name][option] == nil) then
-					if (info.value == nil) then
-						info.value = {}
-					end
 
-					svc.profile[settings.name][option] = info.value
+				-- Master Call (slider = bdConfigLib.SliderElement(config, module, option, info))
+				local method = string.lower(info.type):gsub("^%l", string.upper).."Element"
+				if (bdConfigLib[method]) then
+					bdConfigLib[method](bdConfigLib, module, option, info)
+				else
+					debug("No module defined for "..method.." in "..settings.name)
 				end
-			end
-
-			-- force blank callbacks if not set
-			info.callback = info.callback or settings.callback or function() return end
-			
-			-- If the very first entry is not a tab, then create a general tab/page container
-			if (info.type ~= "tab" and #module.tabs == 0) then
-				module:CreateTab("General")
-			end
-
-			-- Master Call (slider = bdConfigLib.SliderElement(config, module, option, info))
-			local method = string.lower(info.type):gsub("^%l", string.upper).."Element"
-			if (bdConfigLib[method]) then
-				bdConfigLib[method](bdConfigLib, module, option, info)
-			else
-				debug("No module defined for "..method.." in "..settings.name)
 			end
 		end
 	end
